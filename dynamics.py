11502c4229134fcc0d22e5b4c6452a80f48a6b16
Mark Stephenson <mark2000stephenson@gmail.com>
2024-12-09 15:06:26 -0700

    Initial commit

---
+++ dynamics.py
@@ -0,0 +1,151 @@
+from dataclasses import dataclass
+from itertools import product
+from typing import *
+
+import numpy as np
+import scipy.linalg
+from scipy.integrate import solve_ivp
+
+mu = 398600
+R_E = 6378
+omega_E = 2 * np.pi / 86400
+
+
+@dataclass
+class CircularTrajectory:
+    radius: float
+    omega: float
+    theta_zero: float
+
+    @classmethod
+    def from_orbit(cls, radius: float, true_anomaly_zero: float):
+        return cls(
+            radius=radius, omega=np.sqrt(mu / radius**3), theta_zero=true_anomaly_zero
+        )
+
+    @property
+    def period(self):
+        return 2 * np.pi / self.omega
+
+    def state_at(self, t: float):
+        return [
+            self.radius * np.cos(self.omega * t + self.theta_zero),
+            -self.radius * self.omega * np.sin(self.omega * t + self.theta_zero),
+            self.radius * np.sin(self.omega * t + self.theta_zero),
+            self.radius * self.omega * np.cos(self.omega * t + self.theta_zero),
+        ]
+
+
+@dataclass
+class EllipticalTrajectory:
+    x0: Sequence[float]
+
+    def propagate(self, t: np.ndarray):
+        def orbit_dynamics(t, state):
+            x, vx, y, vy = state
+            r = np.sqrt(x**2 + y**2)
+            ax = -mu * x / r**3
+            ay = -mu * y / r**3
+            return [vx, ax, vy, ay]
+
+        return solve_ivp(
+            orbit_dynamics, [t[0], t[-1]], self.x0, t_eval=t, rtol=1e-9
+        ).y.T
+
+
+@dataclass
+class LinearizedSystem:
+    nominal: CircularTrajectory
+    stations: Sequence[CircularTrajectory]
+
+    def A(self, t: float):
+        x = self.nominal.state_at(t)
+        r = np.sqrt(x[0] ** 2 + x[2] ** 2)
+        r5 = r**5
+
+        A = np.zeros([4, 4])
+        A[0, 1] = 1
+        A[2, 3] = 1
+
+        A[1, 0] = (2 * x[0] ** 2 - x[2] ** 2) * mu / r5
+        A[3, 2] = (2 * x[2] ** 2 - x[0] ** 2) * mu / r5
+        A[1, 2] = A[3, 0] = 3 * mu * x[0] * x[2] / r5
+
+        return A
+
+    def B(self, t: float):
+        B = np.zeros([4, 2])
+        B[1, 0] = 1
+        B[3, 1] = 1
+        return B
+
+    def C_i(self, i: int, t: float):
+        C = np.zeros([3, 4])
+
+        x = self.nominal.state_at(t)
+        x_s = self.stations[i].state_at(t)
+        rho = np.sqrt((x[0] - x_s[0]) ** 2 + (x[2] - x_s[2]) ** 2)
+        N = (x[0] - x_s[0]) * (x[1] - x_s[1]) + (x[2] - x_s[2]) * (x[3] - x_s[3])
+
+        C[0, 0] = C[1, 1] = (x[0] - x_s[0]) / rho
+        C[0, 2] = C[1, 3] = (x[2] - x_s[2]) / rho
+
+        C[1, 0] = ((x[1] - x_s[1]) * rho - N * C[0, 0]) / rho**2
+        C[1, 2] = ((x[3] - x_s[3]) * rho - N * C[0, 2]) / rho**2
+
+        det = 1 + ((x[2] - x_s[2]) / (x[0] - x_s[0])) ** 2
+        C[2, 0] = -(x[2] - x_s[2]) / (x[0] - x_s[0]) ** 2 / det
+        C[2, 2] = 1 / (x[0] - x_s[0]) / det
+
+        return C
+
+    def D_i(self, i: int, t: float):
+        return np.zeros([3, 2])
+
+    def C(self, t: float):
+        return np.vstack([self.C_i(i, t) for i in range(len(self.stations))])
+
+    def D(self, t: float):
+        return np.zeros([3 * len(self.stations), 2])
+
+    def F_G(self, t: float, dt: float):
+        A = self.A(t)
+        B = self.B(t)
+        A_hat = np.zeros([A.shape[1] + B.shape[1]] * 2)
+        A_hat[: A.shape[1], : A.shape[1]] = A
+        A_hat[: A.shape[1], A.shape[1] :] = B
+        Z = scipy.linalg.expm(A_hat * dt)
+        return Z[: A.shape[1], : A.shape[1]], Z[A.shape[1] :, : A.shape[1]]
+
+    def H(self, t: float):
+        return self.C(t)
+
+    def M(self, t: float):
+        return self.D(t)
+
+
+def measurement(x: Sequence[float], x_s: Sequence[float]):
+    rho = np.sqrt((x[0] - x_s[0]) ** 2 + (x[2] - x_s[2]) ** 2)
+    N = (x[0] - x_s[0]) * (x[1] - x_s[1]) + (x[2] - x_s[2]) * (x[3] - x_s[3])
+    psi = np.arctan2((x[2] - x_s[2]), (x[0] - x_s[0]))
+    return [rho, N / rho, psi]
+
+
+def measurements(x: Sequence[float], stations: Sequence[CircularTrajectory], t: float):
+    return np.concatenate([measurement(x, st.state_at(t)) for st in stations])
+
+
+def is_station_visible(x: Sequence[float], x_s: Sequence[float]):
+    psi = measurement(x, x_s)[2]
+    theta = np.arctan2(x_s[2], x_s[0])
+    return -np.pi / 2 + theta < psi < np.pi / 2 + theta
+
+
+def mask_non_visible(
+    t: np.ndarray, x: np.ndarray, y: np.ndarray, stations: Sequence[CircularTrajectory]
+):
+    y_copy = np.copy(y)
+    for i, j in product(range(x.shape[0]), range(len(stations))):
+        if not is_station_visible(x[i, :], stations[j].state_at(t[i])):
+            y_copy[i, j * 3 : (j + 1) * 3] = np.nan
+    return y_copy
---
+++ problem3.py
@@ -0,0 +1,223 @@
+import matplotlib.patches as patches
+import matplotlib.pyplot as plt
+import numpy as np
+import numpy.testing as npt
+
+from dynamics import *
+
+dt = 10
+
+nominal_trajectory = CircularTrajectory.from_orbit(radius=6678, true_anomaly_zero=0)
+
+station_trajectories = [
+    CircularTrajectory(radius=R_E, omega=omega_E, theta_zero=i * np.pi / 6)
+    for i in range(12)
+]
+
+lt = LinearizedSystem(nominal_trajectory, station_trajectories)
+
+
+def plot_orbits(xs: Sequence[np.ndarray], ax=None):
+    if ax is None:
+        fig, ax = plt.subplots()
+
+    ax.add_patch(patches.Circle((0, 0), R_E, color="b"))
+    ax.axis("equal")
+    ax.set_xlabel("$x_1$")
+    ax.set_xlabel("$x_3$")
+    return ax, [ax.plot(x[:, 0], x[:, 2])[0] for x in xs]
+
+
+def plot_states(
+    xs: Sequence[np.ndarray],
+    t: Optional[np.ndarray] = None,
+    ylables: Sequence[str] = [],
+    xlabel: str = "",
+    legend_labels: Sequence[str] = [],
+    kwargs: Sequence[dict[str, Any]] = [],
+    axs=None,
+):
+    if axs is None:
+        fig, axs = plt.subplots(xs[0].shape[1])
+
+    if t is None:
+        t = np.arange(xs[0].shape[1])
+
+    lines = np.empty([len(axs), len(xs)], dtype=object)
+    for i, ax in enumerate(axs):
+        for j, x in enumerate(xs):
+            lines[i, j] = axs[i].plot(t, x[:, i], **(kwargs[j] if kwargs else {}))[0]
+
+        if ylables:
+            ax.set_ylabel(ylables[i])
+
+        if ax is not axs[-1]:
+            ax.set_xticklabels([])
+
+    if xlabel:
+        axs[-1].set_xlabel(xlabel)
+
+    if legend_labels:
+        axs[0].legend(
+            lines[0, :],
+            legend_labels,
+            bbox_to_anchor=(0, 1.02, 1, 0.102),
+            loc="lower center",
+            ncols=6,
+        )
+
+    return axs, lines
+
+
+def plot_measurements(
+    y: np.ndarray,
+    t: Optional[np.ndarray] = None,
+    ylables: Sequence[str] = [],
+    xlabel: str = "",
+):
+    n_stations = y.shape[1] // 3
+    ys = [y[:, i * 3 : (i + 1) * 3] for i in range(n_stations)]
+    cmap = plt.get_cmap("tab20")
+    kwargs = [{"marker": "o", "color": cmap(i)} for i in range(n_stations)]
+    return plot_states(
+        ys,
+        t,
+        ylables,
+        xlabel=xlabel,
+        legend_labels=map(str, range(1, n_stations + 1)),
+        kwargs=kwargs,
+    )
+
+
+def test_part_1():
+    ts = np.arange(0, 1400 * dt, step=dt)
+
+    x_nom = np.array([nominal_trajectory.state_at(t) for t in ts])
+    y_nom = np.array(
+        [measurements(x, station_trajectories, t) for t, x in zip(ts, x_nom)]
+    )
+
+    x_hat = np.zeros_like(x_nom)
+    x_hat[0, :] = [0, 0.075, 0, -0.021]  # initial perturbation
+    for i in range(x_hat.shape[0] - 1):
+        F, G = lt.F_G(ts[i], dt)
+        x_hat[i + 1, :] = F @ x_hat[i, :]
+
+    y_hat = np.zeros_like(y_nom)
+    for i in range(x_hat.shape[0]):
+        y_hat[i, :] = lt.H(ts[i]) @ x_hat[i, :]
+
+    x_pert_nonlinear = EllipticalTrajectory(x_nom[0, :] + x_hat[0, :]).propagate(ts)
+    y_pert_nonlinear = np.array(
+        [measurements(x, station_trajectories, t) for t, x in zip(ts, x_pert_nonlinear)]
+    )
+
+    x_pert_linear = x_nom + x_hat
+    y_pert_linear = y_nom + y_hat
+
+    nonlinear_control_points = {7250: [-3557, -6.481, 5651, -4.169]}
+    linear_pert_control_points = {7410: [-82.61, 0.1607, -72.49, -0.1582]}
+    linear_control_points = {9620: [1371, 7.685, -6462, 1.542]}
+
+    for control_points, to_check, atol in [
+        (nonlinear_control_points, x_pert_nonlinear, [1, 0.001, 1, 0.001]),
+        (linear_pert_control_points, x_hat, [0.01, 0.001, 0.01, 0.001]),
+        (linear_control_points, x_pert_linear, [1, 0.001, 1, 0.001]),
+    ]:
+        for t, truth in control_points.items():
+            i = np.where(ts == t)[0][0]
+            for j in range(4):
+                npt.assert_allclose(to_check[i, j], truth[j], atol=atol[j], err_msg=j)
+
+
+def part_1_ex_3():
+    ts = np.arange(0, 14000, step=dt)
+
+    x_nom = np.array([nominal_trajectory.state_at(t) for t in ts])
+    y_nom = np.array(
+        [measurements(x, station_trajectories, t) for t, x in zip(ts, x_nom)]
+    )
+
+    x_hat = np.zeros_like(x_nom)
+    x_hat[0, :] = [0, 0.075, 0, -0.021]  # initial perturbation
+    for i in range(x_hat.shape[0] - 1):
+        F, G = lt.F_G(ts[i], dt)
+        x_hat[i + 1, :] = F @ x_hat[i, :]
+
+    y_hat = np.zeros_like(y_nom)
+    for i in range(x_hat.shape[0]):
+        y_hat[i, :] = lt.H(ts[i]) @ x_hat[i, :]
+
+    x_pert_nonlinear = EllipticalTrajectory(x_nom[0, :] + x_hat[0, :]).propagate(ts)
+    y_pert_nonlinear = np.array(
+        [measurements(x, station_trajectories, t) for t, x in zip(ts, x_pert_nonlinear)]
+    )
+
+    x_pert_linear = x_nom + x_hat
+    y_pert_linear = y_nom + y_hat
+
+    xs = [x_nom, x_pert_nonlinear, x_pert_linear]
+    xs_labels = ["Nominal", "Perturbed (Non-Lin)", "Perturbed (Lin)"]
+    ys_1 = [y_nom[:, :3], y_pert_nonlinear[:, :3], y_pert_linear[:, :3]]
+
+    # ax, lines = plot_orbits(xs)
+    # ax.legend(lines, xs_labels )
+
+    plot_states(
+        [x_hat],
+        ts,
+        ylables=[f"$x_{i}$" for i in range(1, 5)],
+        xlabel="Time [s]",
+        legend_labels=[r"$\bar{x}$"],
+    )
+
+    plot_states(
+        xs,
+        ts,
+        ylables=[f"$x_{i}$" for i in range(1, 5)],
+        xlabel="Time [s]",
+        legend_labels=xs_labels,
+        kwargs=[{"linestyle": "-"}, {"linestyle": "--"}, {"linestyle": ":"}],
+    )
+
+    plot_states(
+        ys_1,
+        ts,
+        ylables=[f"$y^{{s=1}}_{i}$" for i in range(1, 4)],
+        xlabel="Time [s]",
+        legend_labels=xs_labels,
+        kwargs=[{"linestyle": "-"}, {"linestyle": "--"}, {"linestyle": ":"}],
+    )
+
+    axs, _ = plot_states(
+        [(x_pert_nonlinear - x_pert_linear)],
+        ts,
+        ylables=[f"$x_{i}$" for i in range(1, 5)],
+        xlabel="Time [s]",
+    )
+    axs[0].figure.suptitle(r"$(x_{nonlin} - x_{lin})$")
+
+    axs, _ = plot_states(
+        [(y_pert_nonlinear[:, :3] - y_pert_linear[:, :3])],
+        ts,
+        ylables=[f"$y^{{s=1}}_{i}$" for i in range(1, 4)],
+        xlabel="Time [s]",
+    )
+    axs[0].figure.suptitle(r"$(x_{nonlin} - x_{lin})$")
+
+    masked_y_pert_nonlinear = mask_non_visible(
+        ts, x_pert_nonlinear, y_pert_nonlinear, station_trajectories
+    )
+    plot_measurements(
+        masked_y_pert_nonlinear,
+        ts,
+        ylables=[f"$y^{{s=1}}_{i}$" for i in range(1, 4)],
+        xlabel="Time [s]",
+    )
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    # test_part_1()
+    part_1_ex_3()
